17ad790668c8322fbed8e81c3badded67669bfb7
morock-it <morock@onionmail.org>
2026-03-18 19:52:55 +0100

    Use Frida for skip SMS Free

--- cli_main.py
+++ cli_main.py
@@ -72,8 +72,8 @@ ROUTINE_ALERT_SKIP_PATTERNS: tuple[str, ...] = (
     # "telegram proxy popup was not confirmed",
     "proxy must be socks5",
     "internet connectivity check failed",
-    "number is already registered",
-    "already linked to telegram",
+    # "number is already registered",
+    # "already linked to telegram",
 )
 
 
